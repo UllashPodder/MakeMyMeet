f1766736fd0b47690ae3ca64c10e83907e5ba758
Ullash Podder(Mac15) <ullash@ergo-ventures.com>
2019-09-03 19:56:06 +0600

    working on tab views

---
+++ app/src/main/java/com/podder/ullash/makemymeet/LandingPages/MyMeetList/MyListPageAdapter.java
@@ -0,0 +1,6 @@
+package com.podder.ullash.makemymeet.LandingPages.MyMeetList;
+
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class MyListPageAdapter extends FragmentPagerAdapter {
+}
